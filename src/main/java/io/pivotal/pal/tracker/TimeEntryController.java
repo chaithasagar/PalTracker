2cbf5a15e78f1836c8bdbd385b9914b07b501e83
sashinpivotal <sashin@pivotal.io>
2018-11-20 16:38:20 -0500

    metrics impl

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,5 +1,8 @@
 package io.pivotal.pal.tracker;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,16 +14,21 @@ import java.util.List;
 public class TimeEntryController {
 
     private TimeEntryRepository repository;
+    private DistributionSummary distributionSummary;
+    private Counter actionCounter;
 
-    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+    public TimeEntryController(TimeEntryRepository timeEntryRepository, MeterRegistry meterRegistry) {
         this.repository=timeEntryRepository;
+        distributionSummary = meterRegistry.summary("timeEntry.summary");
+        actionCounter = meterRegistry.counter("timeEntry.actionCounter");
 
     }
 
     @PostMapping
     public ResponseEntity create(@RequestBody  TimeEntry timeEntryToCreate) {
         timeEntryToCreate =  repository.create(timeEntryToCreate);
-
+        actionCounter.increment();
+        distributionSummary.record(repository.list().size());
         ResponseEntity response = new ResponseEntity(timeEntryToCreate,HttpStatus.CREATED);
         return response;
 
@@ -32,6 +40,7 @@ public class TimeEntryController {
         ResponseEntity response;
         TimeEntry timeEntry = repository.find(timeEntryId);
         if(timeEntry != null) {
+            actionCounter.increment();
              response = new ResponseEntity(timeEntry, HttpStatus.OK);
         }else{
              response = new ResponseEntity(timeEntry, HttpStatus.NOT_FOUND);
@@ -41,6 +50,7 @@ public class TimeEntryController {
 
     @GetMapping
     public ResponseEntity<List<TimeEntry>> list() {
+        actionCounter.increment();
         List<TimeEntry> timeEntryList = repository.list();
         ResponseEntity response = new ResponseEntity(timeEntryList,HttpStatus.OK);
         return response;
@@ -51,6 +61,7 @@ public class TimeEntryController {
         TimeEntry timeEntry =  repository.update(timeEntryId,expected);
         ResponseEntity response;
         if(timeEntry != null) {
+            actionCounter.increment();
             response = new ResponseEntity(timeEntry, HttpStatus.OK);
         }else{
             response = new ResponseEntity(timeEntry, HttpStatus.NOT_FOUND);
@@ -60,8 +71,9 @@ public class TimeEntryController {
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity delete(@PathVariable("id")  long timeEntryId) {
+        actionCounter.increment();
         TimeEntry timeEntry = repository.delete(timeEntryId);
-
+        distributionSummary.record(repository.list().size());
         ResponseEntity response = new ResponseEntity(timeEntry,HttpStatus.NO_CONTENT);
 return response;
     }
